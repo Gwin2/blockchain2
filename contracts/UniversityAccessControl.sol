e8ff19397e30eb3d51d482170f290678ccfcd29b
Gwin232 <snytkinaleksandr5@mail.ru>
2024-12-02 23:19:17 +0700

    first commit

--- contracts/UniversityAccessControl.sol
+++ contracts/UniversityAccessControl.sol
@@ -7,10 +7,12 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 contract UniversityAccessControl is Initializable, AccessControlUpgradeable {
     
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant STUDENT_ROLE = keccak256("STUDENT_ROLE");
+    bytes32 public constant TEACHER_ROLE = keccak256("TEACHER_ROLE");
 
     function initialize() public initializer {
         __AccessControl_init();
-        _setupRole(ADMIN_ROLE, msg.sender);
+        _setupRole(msg.sender, ADMIN_ROLE);
     }
 
     enum Role { None, Student, Teacher, Admin }
@@ -22,35 +24,33 @@ contract UniversityAccessControl is Initializable, AccessControlUpgradeable {
         _;
     }
 
-    function assignRole(address _account, Role _role) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function assignRole(address _account, Role _role) external onlyRole(ADMIN_ROLE) {
         require(_role != Role.None, "Invalid role");
         _grantRole(getRoleHash(_role), _account);
         emit RoleAssigned(_account, _role);
     }
 
-    function addUser(address _account, Role _role) external onlyRole(Role.Admin) {
+    function addUser(address _account, Role _role) external onlyRole(ADMIN_ROLE) {
         require(_role != Role.None, "Invalid role");
-        _grantRole(_role, _account);
+        _grantRole(getRoleHash(_role), _account);
         emit RoleAssigned(_account, _role);
     }
 
+    function getRoleHash(Role _role) internal pure returns (bytes32) {
+        if (_role == Role.Student) return STUDENT_ROLE;
+        if (_role == Role.Teacher) return TEACHER_ROLE;
+        if (_role == Role.Admin) return ADMIN_ROLE;
+        return keccak256("");
+    }
+
+    function hasRole(Role _role, address _account) public view returns (bool) {
+        return hasRole(getRoleHash(_role), _account);
+    }
+
     function getRole(address _account) external view returns (Role) {
-        uint256 role = getRoleHash(_account);
-        if (role == getRoleHash(Role.Student)) return Role.Student;
-        if (role == getRoleHash(Role.Teacher)) return Role.Teacher;
-        if (role == getRoleHash(Role.Admin)) return Role.Admin;
+        if (hasRole(getRoleHash(Role.Student), _account)) return Role.Student;
+        if (hasRole(getRoleHash(Role.Teacher), _account)) return Role.Teacher;
+        if (hasRole(getRoleHash(Role.Admin), _account)) return Role.Admin;
         return Role.None;
-    }
-
-    function hasRole(address _account, Role _role) public view returns (bool) {
-        return hasRole(_role, _account);
-    }
-
-    function getRoleHash(address _account) internal view returns (uint256) {
-        return uint256(keccak256(abi.encodePacked(_account)));
-    }
-
-    function getRoleHash(Role _role) internal pure returns (uint256) {
-        return uint256(keccak256(abi.encodePacked(_role)));
     }
 }
